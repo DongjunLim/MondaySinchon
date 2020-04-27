b9785556d45be2e5fce481c54ecc3ec7f036b59f
DongjunLim <armada55@kpu.ac.kr>
2020-04-27 22:34:34 +0900

    update dongjun's code

--- 2주차-백트래킹/백트래킹-1759-암호만들기/dongjun.py
+++ 2주차-백트래킹/백트래킹-1759-암호만들기/dongjun.py
@@ -1,12 +1,11 @@
 import sys
-sys.stdin = open('./input.txt','r')
 
 # 비밀번호에 모음 1개, 자음 2개인지 확인하기 위한 모음배열
 consonant = ['a','e','u','i','o']
 
 answer = []
 
-# 비밀번호 길이 L과 후보알파벳 갯수 C를 입력받음 
+# 비밀번호 길이 L과 후보알파벳 갯수 C를 입력받음
 L ,C = map(int, input().split())
 
 # 후보알파벳 문자를 배열로 입력받음
@@ -20,15 +19,16 @@ def find(idx, string):
     global L
     global answer
     global arr
-    
+    global last
+
     # 비밀번호를 다 만들었으면 비밀번호 조건에 해당하는지 확인하고 해당하면 정답배열에 push하고 리턴
     if idx == L:
         if checkCondition(string):
             answer.append(string)
         return
-    
+    lastIdx = last+idx
     # 오름차순으로 정렬된 arr배열을 idx 번부터 끝까지 순회
-    for x in arr[idx:]:
+    for x in arr[idx:lastIdx]:
         # 오름차순으로 만들기 위해 만들고 있는 비밀번호의 가장 끝자리보다 입력값이 큰지 확인한다.
         if string[-1] < x:
             string += x
@@ -54,9 +54,9 @@ def checkCondition(string):
 
 # 비밀번호를 만들때 사용할 문자열 변수
 string = ''
-
+last = C-L+1
 # 첫 문자는 for문으로 입력하고 그 이후부터 find 내에서 추가한다.
-for x in arr:
+for x in arr[:last]:
     string += x
     find(1,string)
     string = string[:-1]
